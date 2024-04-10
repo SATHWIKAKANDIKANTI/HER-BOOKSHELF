ab173f515f221f8a28a5c0ae6fd7d22caf789a7e
SATHWIKAKANDIKANTI <149418928+SATHWIKAKANDIKANTI@users.noreply.github.com>
2024-04-10 13:49:36 +0530

    Add files via upload

---
+++ writers.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+ 
+    $("#cb").click(function(){
+      window.location.href = "https://www.amazon.in/Books-Charlotte-Bronte/s?rh=n%3A976389031%2Cp_27%3ACharlotte+Bronte";
+    });
+    $("#tm").click(function(){
+      window.location.href = "https://www.amazon.in/Books-Toni-Morrison/s?rh=n%3A976389031%2Cp_27%3AToni+Morrison";
+    });
+    $("#kd").click(function(){
+        window.location.href = "https://www.amazon.in/Books-Kiran-Desai/s?rh=n%3A976389031%2Cp_27%3AKiran+Desai";
+      });
+      $("#ar").click(function(){
+        window.location.href = "https://www.amazon.in/Books-Arundhati-Roy/s?rh=n%3A976389031%2Cp_27%3AArundhati+Roy";
+      });
+      $("#sm").click(function(){
+        window.location.href = "https://www.amazon.in/Books-Sudha-Murty/s?rh=n%3A976389031%2Cp_27%3ASudha+Murty";
+      });
+     
+  });
